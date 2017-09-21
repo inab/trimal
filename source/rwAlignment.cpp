cdcb7ec47f3673da08424c84903f975954bb38fb
scapella <salcagu@gmail.com>
2017-09-21 16:30:01 +0200

    Fix a minor bug which included as part of the sequence the termination symbol '*' when reading PIR/NBRF format files.

--- source/rwAlignment.cpp
+++ source/rwAlignment.cpp
@@ -1296,7 +1296,7 @@ bool alignment::loadNBRF_PirAlignment(char *alignmentFile) {
   /* NBRF/PIR file format parser */
 
   bool seqIdLine, seqLines;
-  char *str, *line = NULL;
+  char *str, *line = NULL; 
   ifstream file;
   int i;
 
@@ -1398,6 +1398,11 @@ bool alignment::loadNBRF_PirAlignment(char *alignmentFile) {
         sequences[i].append(str, strlen(str));
         str = strtok(NULL, OTHDELIMITERS);
       }
+
+      /* In case the end symbol '*' has been detected, remove it */
+      if(sequences[i][sequences[i].size() - 1] == '*')
+        sequences[i].erase(sequences[i].size()-1);
+
     }
   }
   /* Close the input file */
