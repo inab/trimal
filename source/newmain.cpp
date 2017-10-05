2995159d25633ea97b31a6ccee149e9d340acb7d
Victor Fernandez <victor.fernandez@bsc.es>
2017-10-05 16:39:45 +0200

    trimAl branch divergence

--- source/newmain.cpp
+++ source/newmain.cpp
@@ -32,6 +32,8 @@ int main(int argc, char *argv[])
     debug.IsDebug = true;
     
 //     debug.Level = VerboseLevel::ERROR;
+    
+    debug.log(VerboseLevel::ERROR) << "This is the origin branch" << endl;
 
     trimAlManager Trimal = trimAlManager();
 
