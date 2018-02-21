4f2c74c23e0b1dc5134a601c97ec67485eba7888
Victor Fernandez <victor.fernandez@bsc.es>
2018-02-21 12:49:22 +0100

    Modified the TimerFactory to be able to benchmark times and memory independently with a directive. Improved the tree output. TODO: More work is needed in the tree output

--- include/TimerFactory.h
+++ include/TimerFactory.h
@@ -5,12 +5,17 @@
 #ifndef MTRIMAL_TIMERFACTORY_H
 #define MTRIMAL_TIMERFACTORY_H
 
-#define TimingReport true
+//#define TimingReport true
+
+#define BenchmarkTimes true
+#define BenchmarkMemory true
+
+#define TimingReport BenchmarkTimes || BenchmarkMemory
 
 #if TimingReport
 
 #define StartTiming(name) auto macroCustomTimer = timerFactory.getTimer(name)
-#define SetTimingOfstream(_filename) timerFactory.out = new std::ofstream(_filename);
+#define SetTimingOfstream(_filename) timerFactory.SetOutput(_filename)
 #include <chrono>
 #include <string>
 #include <stack>
@@ -23,15 +28,31 @@ class TimerFactory;
 
 class Timer {
     friend TimerFactory;
+
+#if BenchmarkTimes
+    /// Creation timestamp
     std::chrono::time_point<std::chrono::high_resolution_clock> start;
+    /// Siblings duration, it serves to remove this time from the own calculations
     std::chrono::milliseconds siblings_duration;
+#endif
+    /// Name of the timer
     std::string name;
+    /// How many childs does this timer have
     int childCount = 0;
-    int currentMemoryUsage();
+
+#if BenchmarkMemory
+    /// Method to chek current memory usage
+    static int currentMemoryUsage();
+#endif
 
 public:
+    static std::string separator, spacer;
+
+    /// Constructor
     explicit Timer(std::string name, TimerFactory *timerFactory);
+    /// TimeFactory pointer
     TimerFactory* timerFactory;
+    /// Destructor
     ~Timer();
 };
 
@@ -40,9 +61,13 @@ public:
 class TimerFactory {
     friend Timer;
 public:
+    /// Class Destructor
     ~TimerFactory();
-    Timer getTimer(std::string name);;
+    /// Method to start timing an scope
+    Timer getTimer(std::string name);
 
+#if BenchmarkTimes
+    /// Class constructor
     TimerFactory(std::string outFilename)
             : lastTimer(""), accTimer(0),
               wholeStats(std::map<std::string, std::chrono::milliseconds>()),
@@ -50,21 +75,44 @@ public:
               total(std::chrono::milliseconds(0)) {
         out = new std::ofstream(outFilename);
     };
+#else
+    TimerFactory(std::string outFilename)
+            : lastTimer("") {
+        out = new std::ofstream(outFilename);
+    };
+#endif
 
+    void SetOutput(std::string _filename)
+    {
+        delete out;
+        out = new std::ofstream(_filename);
+    }
+    /// Method to stop tracking and report
     void reportTotal();
 
-    std::ostream * out;
 private:
-
+    /// Timer Level
     int timer = 1;
+    /// Timer Pool
     std::vector<Timer *> _pool;
+    /// Last Timer name. It serves to check if we are repeating the same step over and over.
     std::string lastTimer;
+#if BenchmarkTimes
+    /// Accumulative timer
     std::chrono::milliseconds accTimer;
+    /// Map that contains the whole timing stats
+    std::map<std::string, std::chrono::milliseconds> wholeStats;
+    /// Map that contains the timing stats of timers, without including it's childs.
+    std::map<std::string, std::chrono::milliseconds> wholeUniqueStats;
+    /// Total time since the start of the first timer
+    std::chrono::milliseconds total;
+#endif
+    /// How many times the same counter has been triggered
     int accTimerCount{0};
 
-    std::map<std::string, std::chrono::milliseconds> wholeStats;
-    std::map<std::string, std::chrono::milliseconds> wholeUniqueStats;
-    std::chrono::milliseconds total;
+    /// Ostream where to output the report
+    std::ostream * out;
+    /// Cursor position of the output file.
     std::streampos cursorPos{0};
 };
 
--- source/TimerFactory.cpp
+++ source/TimerFactory.cpp
@@ -8,19 +8,38 @@
 #include <reportsystem.h>
 
 #if TimingReport
-TimerFactory timerFactory = TimerFactory("");
+TimerFactory timerFactory = TimerFactory(""); // NOLINT
 
+//region TimerFactory
 Timer TimerFactory::getTimer(std::string name) {
+    // Create a new timer
     Timer timer = Timer(std::move(name), this);
+    // Store its pointer in the current queue
     _pool.push_back(&timer);
+    // Return the timer
     return timer; // NOLINT
 }
 
 void TimerFactory::reportTotal() {
+
+    *out << "\n"
+         << Timer::spacer
+         << "'~ "
+         #if BenchmarkTimes
+         << " Duration: " << total.count() << " ms."
+         #endif
+         #if BenchmarkMemory
+         << " VmRSS: " << Timer::currentMemoryUsage() << " kb"
+#endif
+            ;
+
+
+#if BenchmarkTimes
+    // Write a separator line to split the tree and the chart.
     *out << "\n";
+    // Calculate the max size of the timer names
     int maxSize = -1;
-    for (auto &&value : wholeStats)
-    {
+    for (auto &&value : wholeStats) {
         maxSize = std::max(maxSize, static_cast<const int &>(value.first.substr(0, value.first.find("(")).length()));
         maxSize = std::max(maxSize, static_cast<const int &>(value.first.substr(value.first.find("(")).length()));
     }
@@ -42,6 +61,7 @@ void TimerFactory::reportTotal() {
             << " " << std::setw(25) << " "
             << " " << std::setw(25) << " "
             << std::setfill(' ')
+
             // Titles
             << "\n"
             << " |" << std::setw(maxSize - 5) << "Key " << " | "
@@ -50,6 +70,7 @@ void TimerFactory::reportTotal() {
             << " |" << std::setw(21) << "Total Duration " << " | "
             << " |" << std::setw(21) << "Rel Total Duration " << " | "
             << "\n"
+
             // Titles separators
             << std::setfill('-')
             << " " << std::setw(maxSize - 1) << " "
@@ -60,113 +81,203 @@ void TimerFactory::reportTotal() {
             << std::setfill(' ')
             << "\n";
 
+    // Foreach Timer triggered
     for (auto &&value : wholeStats) {
+        // output the name
         *out << std::setw(maxSize - 2) << value.first.substr(0, value.first.find("(")) << "  "
+             // Output the unique timing
              << std::setw(21) << wholeUniqueStats[value.first].count() << " ms "
              << std::fixed
+             // Output the relative unique timing
              << std::setw(21) << wholeUniqueStats[value.first].count() * 100.F / total.count() << " %  "
              << std::defaultfloat
+             // Output the total timing
              << std::setw(21) << value.second.count() << " ms "
              << std::fixed
+             // Output the relative total timing
              << std::setw(21) << value.second.count() * 100.F / total.count() << " %  "
              << std::defaultfloat
+             // Output the parameters of the method
              << "\n"
-             << std::setw(maxSize - 2) << value.first.substr(value.first.find("(")) << "\n"
-                ;
+             << std::setw(maxSize - 2) << value.first.substr(value.first.find("(")) << "\n";
     }
+#endif
 }
 
 TimerFactory::~TimerFactory() {
+
+    if (!_pool.empty()) {
+        delete _pool.back();
+    }
+
+
+    // Output the chart
     reportTotal();
+    // Delete the out pointer in case it's not a global one (cout, cerr, clog)
     if (out->rdbuf() != std::cout.rdbuf() &&
         out->rdbuf() != std::cerr.rdbuf() &&
         out->rdbuf() != std::clog.rdbuf())
         delete out;
 }
+//endregion
+
+//region Timer
+
+std::string Timer::separator = std::string("|");
+std::string Timer::spacer = std::string("  ");
+
 
 Timer::Timer(std::string name, TimerFactory *timerFactory) :
-        name(std::move(name)), timerFactory(timerFactory), siblings_duration(0) {
+        name(std::move(name)),
+        timerFactory(timerFactory)
+#if BenchmarkTimes
+        , siblings_duration(0)
+#endif
+{
 
+#if BenchmarkTimes
+    // Save the current timestamp
     start = std::chrono::high_resolution_clock::now();
-
-    if (!timerFactory->_pool.empty())
-    {
+#endif
+    // If the Timer Manager has a current timer
+    if (!timerFactory->_pool.empty()) {
+        // We add a child to the last timer
+        // This means that hierarchically we are a child of the previous timer
         timerFactory->_pool.back()->childCount += 1;
     }
 
+    // We check if the last finished timer is not the same as the current one
     if (timerFactory->lastTimer != this->name) {
+        // We add a new line
         *timerFactory->out << "\n";
+        // And output the tree form
         for (int i = 0; i < timerFactory->timer - 1; i++) {
-            *timerFactory->out << "|";
+            *timerFactory->out << spacer << separator;
         }
-        *timerFactory->out << "|----> 1.";
-        for (Timer* timer : timerFactory->_pool)
-        {
+        // We output the numbered ID
+        *timerFactory->out << spacer << separator << "----> ";
+        for (Timer *timer : timerFactory->_pool) {
             *timerFactory->out << timer->childCount << ".";
         }
-
-        *timerFactory->out << "  " << this->name << " " << currentMemoryUsage() << " kb";
+        // And output the current memory located.
+        *timerFactory->out << "  " << this->name;
     }
+
+    // Increase the number of timers we have.
     timerFactory->timer++;
-}
 
-int Timer::currentMemoryUsage() {
-    std::ifstream proc_status_fhandle("/proc/self/status");
-    std::string s;
-    int line=0;
-    while(std::getline(proc_status_fhandle, s)){
-        ++line;
-        if (!s.compare(0, 6, "VmRSS:"))
-        {
-            int value = atoi(&(s.substr(7, s.npos))[0]);
-//            debug << s << "\n";
-            return value;
-        }
-    }
 }
 
 Timer::~Timer() {
+    // Reduce the number of timers out Manager has
     timerFactory->timer--;
+#if BenchmarkTimes
+    // Calculate the diff of timestamps since creation
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(
             std::chrono::high_resolution_clock::now() - start);
-    if (timerFactory->lastTimer == name) {
+#endif
+    // Check if the last timer finished is the same as current.
+    if (timerFactory->lastTimer == name && childCount == 0) {
+        // Increase the counter of times we've called the same timer in different calls
         timerFactory->accTimerCount += 1;
+#if BenchmarkTimes
+        // Increase the counter of time ellapsed within the same timer in different calls
         timerFactory->accTimer += duration;
+#endif
+        // Move the position to the cursorPos, to remove the last call.
+        // The cursorPos will be the same on each repetition
         timerFactory->out->seekp(timerFactory->cursorPos);
-        for (int i = 0; i < timerFactory->timer; i++) *timerFactory->out << "|";
-        *timerFactory->out << "`·~ " << name
+        // Add the tree separators
+        for (int i = 0; i < timerFactory->timer; i++) *timerFactory->out << spacer << separator;
+        // Output the timing info: Name, Repetitions, Average and Accumulative.
+        *timerFactory->out << spacer
+                           << "'~ " << name
                            << " Repetitions: " << timerFactory->accTimerCount
+                           #if BenchmarkTimes
                            << " Average: " << (timerFactory->accTimer.count() / timerFactory->accTimerCount) << "ms."
-                           << " Acc " << timerFactory->accTimer.count() << "ms. "
-                            << currentMemoryUsage() << " kb";
+                           << " Acc " << timerFactory->accTimer.count() << "ms."
+                           #endif
+                           #if BenchmarkMemory
+                           << " VmRSS: " << currentMemoryUsage() << " kb";
+#else
+        ;
+#endif
 
     } else {
+        // Set the counter of times we've called the same timer in different calls as 1
         timerFactory->accTimerCount = 1;
+#if BenchmarkTimes
+        // Set the counter of time elapsed within the same timer in different calls to the current timer duration
         timerFactory->accTimer = duration;
+#endif
+        // Set the last finished timer name as the current timer name
         timerFactory->lastTimer = name;
 
+        // Output will be on the nex line.
         *timerFactory->out << "\n";
+        // We store the current cursor position
         timerFactory->cursorPos = timerFactory->out->tellp();
-        for (int i = 0; i < timerFactory->timer; i++) *timerFactory->out << "|";
-        *timerFactory->out << "`·~ " << name
-                           << " Duration: " << (duration - siblings_duration).count() << "ms."
-                           << " Acc " << duration.count() << "ms. "
-                           << currentMemoryUsage() << " kb";
+        // Output the separator
+        for (int i = 0; i < timerFactory->timer; i++) *timerFactory->out << spacer << separator;
+        // Output the timing info: Name, Duration, Accumulative
+        *timerFactory->out << spacer
+                           << "'~ " << name
+                           #if BenchmarkTimes
+                           << " Duration: " << (duration - siblings_duration).count() << " ms."
+                           << " Acc: " << duration.count() << " ms."
+                           #endif
+                           #if BenchmarkMemory
+                           << " VmRSS: " << currentMemoryUsage() << " kb";
+#else
+        ;
+#endif
+        // Output a new line
         *timerFactory->out << "\n";
-        for (int i = 0; i < timerFactory->timer; i++) *timerFactory->out << "|";
+        // Output the tree separators
+        for (int i = 0; i < timerFactory->timer; i++) *timerFactory->out << spacer << separator;
     }
+
+    // Remove the current pointer from que queue.
     timerFactory->_pool.pop_back();
+#if BenchmarkTimes
+    // If we are inside another timer, add the current duration to it's sibilings duration
     if (!timerFactory->_pool.empty()) {
         timerFactory->_pool.back()->siblings_duration += duration;
     }
     if (timerFactory->wholeStats.find(name) != timerFactory->wholeStats.end()) {
         timerFactory->wholeStats[name] += duration;
         timerFactory->wholeUniqueStats[name] += (duration - siblings_duration);
-    } else {
+        // If it's the first time we trigger this timer, add it to the timing maps.
+    }
+        // If not, increase its duration
+    else {
         timerFactory->wholeStats[name] = duration;
         timerFactory->wholeUniqueStats[name] = (duration - siblings_duration);
     }
+    // Increase the total duration of the program.
     timerFactory->total += (duration - siblings_duration);
+#endif
+
 }
+
+#if BenchmarkMemory
+
+int Timer::currentMemoryUsage() {
+    static std::streampos VmRSScursor = -1;
+    std::ifstream proc_status_fhandle("/proc/self/status");
+    std::string s;
+    int line = 0;
+    while (std::getline(proc_status_fhandle, s)) {
+        ++line;
+        if (!s.compare(0, 6, "VmRSS:")) {
+            int value = atoi(&(s.substr(7, s.npos))[0]);
+            return value;
+        }
+    }
+
+}
+
+#endif
+//endregion
 
 #endif
