dbc92a971f24477e345e45d7dd31f6f4c5f4be57
Victor Fernandez <victor.fernandez@bsc.es>
2019-02-08 15:43:19 +0100

    Changed the Format Hander Overwrite Policy Script so it affects all targets in project, not only trimAl. More work is needed to make it fully functional.

--- scripts/CMake/FormatHandlerOverwritePolicy.cmake
+++ scripts/CMake/FormatHandlerOverwritePolicy.cmake
@@ -7,12 +7,12 @@ cmake_minimum_required(VERSION 3.0.2)
 set(shouldOverwrite true CACHE BOOL
         "Whether trimAl and readAl should overwrite existant files or rename the output adding '.x' suffix where x is a integer.")
 
-target_compile_definitions(trimal PUBLIC FormatHandlerOverwrites=${shouldOverwrite})
+add_compile_definitions(FormatHandlerOverwrites=${shouldOverwrite})
 
 set(mustOverwriteOriginal true CACHE BOOL
         "Whether trimAl and readAl should overwrite the first (true) or the last(false) element if overwrite is needed.")
 
-target_compile_definitions(trimal PUBLIC FormatHandlerOverwritesOriginal=${mustOverwriteOriginal})
+add_compile_definitions(FormatHandlerOverwritesOriginal=${mustOverwriteOriginal})
 
 message(STATUS "Will override original file if existent? ${shouldOverwrite}.")
 if (NOT ${shouldOverwrite})
