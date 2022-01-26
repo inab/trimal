997f245417c937b5c1dae249617c4d35e074b1b1
Nicolás Díaz Roussel <ndiazrou@bsc.es>
2022-01-26 16:24:28 +0100

    Count number of ungapped columns

--- scripts/get_statistics.sh
+++ scripts/get_statistics.sh
@@ -8,6 +8,7 @@ cd $test_working_files
 > blocks_outputs.txt
 > number_sequences.txt
 > number_columns.txt
+> gap_stats.txt
 for file in $dataset*.fasta;
 do
     echo $file
@@ -16,5 +17,12 @@ do
         python3 $scripts/set_manual_boundaries.py -i temporal_out.txt --inner_blocks --total_blocks --min_gapscore_allowed 1 >> blocks_outputs.txt
         grep ">" $file | wc -l >> number_sequences.txt
         trimal -in $file -sgc | tail -n 1 | grep -E "^\s*[[:digit:]]{1,}" -o | awk '{print $1 + 1}' >> number_columns.txt
+        trimal -in $file -sgt > temporal_out.txt
+        gap_score=$(grep -E "[[:digit:]]" temporal_out.txt -m 1 -o | tail -n 1)
+        if [ $gap_score == 1 ]; then
+            grep -E "[[:digit:]]{1,}" temporal_out.txt -m 1 -o | head -n 1 >> gap_stats.txt
+        else
+            echo 0 >> gap_stats.txt
+        fi
     fi
 done
