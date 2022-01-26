255fcab5ad7bbb984fa18d44717c80bfd593fdfb
Nicolás Díaz Roussel <ndiazrou@bsc.es>
2022-01-26 16:24:54 +0100

    Read new computed parameters

--- scripts/measure_parameters_relevance.py
+++ scripts/measure_parameters_relevance.py
@@ -18,6 +18,7 @@
 #   more details on <http://www.gnu.org/licenses/>
 #
 
+from ctypes import alignment
 import os
 import sys
 import argparse
@@ -25,28 +26,88 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.preprocessing import MinMaxScaler
+import random
 
 def main():
-    df = pd.read_csv('https://raw.githubusercontent.com/datagy/data/main/insurance.csv')
-    print(df.head())
-    print(df.info())
-    print(df.corr())
+  '''
+  df = pd.read_csv('https://raw.githubusercontent.com/datagy/data/main/insurance.csv')
+  print(df.head())
+  print(df.info())
+  print(df.corr())
 
-    model = LinearRegression()
-    non_smokers = df[df['smoker'] == 'no']
-    X = non_smokers[['age', 'bmi']]
-    y = non_smokers['charges']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, shuffle=True, train_size=0.3)
-    model.fit(X_train, y_train)
+  model = LinearRegression()
+  non_smokers = df[df['smoker'] == 'no']
+  X = non_smokers[['age', 'bmi']]
+  y = non_smokers['charges']
+  X_train, X_test, y_train, y_test = train_test_split(X, y, shuffle=True, train_size=0.3)
+  model.fit(X_train, y_train)
 
-    predictions = model.predict(X_test)
-    r2 = r2_score(y_test, predictions)
-    rmse = mean_squared_error(y_test, predictions, squared=False)
+  predictions = model.predict(X_test)
+  r2 = r2_score(y_test, predictions)
+  rmse = mean_squared_error(y_test, predictions, squared=False)
 
-    print('The r2 is: ', r2)
-    print('The rmse is: ', rmse)
-    print(model.coef_)
-    print(model.intercept_)
+  print('The r2 is: ', r2)
+  print('The rmse is: ', rmse)
+  print(model.coef_)
+  print(model.intercept_)
+  '''
+
+  '''
+  alignment_index = 0
+  with open(r'test_working_files/gap_stats.txt', 'r') as infile, \
+     open(r'test_working_files/gap_stats_clean.txt', 'w') as outfile:
+    data = infile.read()
+    data = data.replace("\t\t", "\t")
+    outfile.write(data)
+
+  df = pd.read_table("test_working_files/gap_stats_clean.txt", index_col=False, skiprows=3, names=["#_residues",
+    "%_length", "cumulative_# residues", "%_cumulative_length", "gaps/column", "%_gaps/column", "gap_score/column"])
+  df["alingment_index"] = alignment_index
+  
+  print(df.head())
+  print(df.info())
+
+  num_colums = sum(df["#_residues"])
+  '''
+
+  
+  # First we'll try with just number of sequences and number of blocks and random output value
+  num_sequences = pd.read_table("test_working_files/number_sequences.txt", header=None, names=["num_sequences"])
+  num_blocks = pd.read_table("test_working_files/blocks_outputs.txt", header=None, names=["num_blocks"])
+  num_columns = pd.read_table("test_working_files/number_columns.txt", header=None, names=["num_columns"])
+  num_ungapped_columns = pd.read_table("test_working_files/gap_stats.txt", header=None, names=["num_ungapped_columns"])
+
+  df = pd.concat([num_sequences, num_blocks, num_columns, num_ungapped_columns], axis=1)
+  df["score"] = [random.random() for i in range(df.shape[0])]
+
+
+  print(df.head())
+  print(df.info())
+  print(df.corr())
+
+  #run_model(df)
+
+
+def run_model(df):
+  scaler = MinMaxScaler()
+  df = pd.DataFrame(scaler.fit_transform(df),
+            columns=df.columns, index=df.index) 
+
+  model = LinearRegression()
+  X = df[['num_sequences', 'num_blocks', 'num_columns']]
+  y = df['score']
+  X_train, X_test, y_train, y_test = train_test_split(X, y, shuffle=True, train_size=0.3)
+  model.fit(X_train, y_train)
+
+  predictions = model.predict(X_test)
+  r2 = r2_score(y_test, predictions)
+  rmse = mean_squared_error(y_test, predictions, squared=False)
+
+  print('The r2 is: ', r2)
+  print('The rmse is: ', rmse)
+  print(model.coef_)
+  print(model.intercept_)
 
 
 if __name__ == "__main__":
