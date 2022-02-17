4f2f5ac8042696325e68f4a364d4518e16c2cbf0
Nicolás Díaz Roussel <ndiazrou@bsc.es>
2022-02-17 09:33:50 +0100

    Put omp directive after comment

--- source/Cleaner.cpp
+++ source/Cleaner.cpp
@@ -1453,8 +1453,8 @@ void Cleaner::calculateSeqIdentity() {
         alig->identities[i][i] = 0;
     }
 
-    #pragma omp parallel for num_threads(NUMTHREADS) private(j, k, hit, dst) if(alig->originalNumberOfSequences>MINPARALLELSIZE)
     // For each seq, compute its identity score against the others in the MSA
+    #pragma omp parallel for num_threads(NUMTHREADS) private(j, k, hit, dst) if(alig->originalNumberOfSequences>MINPARALLELSIZE)
     for (i = 0; i < alig->originalNumberOfSequences; i++) {
         if (alig->saveSequences[i] == -1) continue;
 
