62a6cede52b0714f769c34730906b8e222775a7e
Nicolás Díaz Roussel <nicolas.diazroussel@bsc.es>
2023-09-21 15:54:26 +0200

    Update conf file

--- docs/conf.py
+++ docs/conf.py
@@ -26,10 +26,3 @@ intersphinx_mapping = {
 intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
-
-# -- Options for HTML output
-
-html_theme = 'sphinx_rtd_theme'
-
-# -- Options for EPUB output
-epub_show_urls = 'footnote'
