b335b7d55dc363670106f128c94487e9d700caa9
Nicolás Díaz Roussel <nicolas.diazroussel@bsc.es>
2024-02-26 10:07:49 +0100

    Fix test script

--- scripts/compare_trimmed_msas.sh
+++ scripts/compare_trimmed_msas.sh
@@ -1,10 +1,11 @@
-methods=(gappyout strict strictplus automated1 nogaps noallgaps)
+methods="gappyout strict strictplus automated1 nogaps noallgaps"
 for method in $methods
 do
     for msa in test_msas/$method/*.fasta
     do
         msa_filename=$(basename $msa)
-        diff $msa dataset/trimmed_msas/$method/$msa_filename -q || exit 1
+        diff "$msa" "dataset/trimmed_msas/$method/$msa_filename" -q || exit 1
+        echo "Compared $msa and dataset/trimmed_msas/$method/$msa_filename"
     done
 done
 rm -rf test_msas
